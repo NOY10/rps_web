6ab5cc7e91c7371aa8d5a2dc4cb8f0b48135d4ad
N0rphel <tnorphel09@gmail.com>
2023-11-01 02:29:33 +0600

    initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  BsFillBellFill,
+  BsFillEnvelopeFill,
+  BsPersonCircle,
+  BsSearch,
+  BsJustify,
+} from "react-icons/bs";
+import Search from "./Search";
+
+function Header({ OpenSidebar }) {
+  return (
+    <header className="header">
+      <div className="menu-icon">
+        <BsJustify className="icon" onClick={OpenSidebar} />
+      </div>
+      <div className="header-left">
+        <Search />
+      </div>
+      <div className="header-right">
+        <BsFillBellFill className="icon" />
+        <BsFillEnvelopeFill className="icon" />
+        <BsPersonCircle className="icon" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Search.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { BsSearch } from "react-icons/bs";
+
+function Search() {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = () => {
+    console.log(searchText);
+  };
+
+  return (
+    <div className="search-bar">
+      <div>
+        <BsSearch className="search-icon" />
+      </div>
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default Search;
